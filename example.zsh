6bfbce49b2dd9e41fa9ee3603dc7f8370a83390d
Yucheng Huang <yuh032@ucsd.edu>
2023-06-20 13:44:44 -0700

    added ohmyzsh and tmux config

---
+++ example.zsh
@@ -0,0 +1,55 @@
+# Put files in this folder to add your own custom functionality.
+# See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
+# 
+# Files in the custom/ directory will be:
+# - loaded automatically by the init script, in alphabetical order
+# - loaded last, after all built-ins in the lib/ directory, to override them
+# - ignored by git by default
+# 
+# Example: add custom/shortcuts.zsh for shortcuts to your local projects
+# 
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+<<<<<<< Updated upstream
+=======
+#
+alias hello='echo hi'
+alias hi='echo hello'
+alias zsh_source='source ~/.oh-my-zsh/oh-my-zsh.sh'
+alias zsh_config='vim ~/.oh-my-zsh/custom/example.zsh'
+# alias c:='cd /mnt/c'
+# alias d:='cd /mnt/d'
+# alias f:='cd /mnt/f'
+# alias i:='cd /mnt/i'
+# alias work='cd /mnt/f/workspace'
+alias pfoo='echo hello'
+alias tmux_boot='sh ~/storage/scripts/tmux/boot_tmux.sh'
+alias tmux_kill='sh ~/storage/scripts/tmux/tmux_kill_all.sh'
+alias tmux_reboot='sh ~/storage/scripts/tmux/tmux_reboot.sh'
+alias net_check='sh ~/storage/scripts/utils/net_check.sh'
+alias nachos='~/workspace/CSE120/nachos_sp22_AidenHuang01_Tianyue-Zhao_yuang-lin/nachos/bin/nachos'
+alias clc='clear'
+alias sshto='~/storage/scripts/ssh/ssh_to.sh'
+alias intj='/home/yucheng/.local/share/JetBrains/Toolbox/apps/IDEA-U/ch-0/221.5080.210/bin/idea.sh'
+alias edge='microsoft-edge'
+alias o='open .'
+
+alias tb='tmux_boot'
+alias tk='tmux_kill'
+alias fv='xdg-open'
+
+alias leetcode='cd ~/.leetcode'
+alias beidanci='python /User/yucheng/workspace/utils/GRE/beidanci.py'
+alias wcsng='cd ~/workspace/WCSNG'
+alias work='cd ~/workspace'
+alias blinder='cd /Users/yucheng/sp23/CSE/group-3'
+
+
+export SPOTIPY_CLIENT_ID='2a407e6bd7524a4c830f7a854f86f516'
+export SPOTIPY_CLIENT_SECRET='dff4b2d7889a48148022b9c31860b815'
+export SPOTIPY_REDIRECT_URI='https://localhost:8888/callback'
+
+ [ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
+
+
+>>>>>>> Stashed changes
