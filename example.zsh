7a2a60d1def8141642911d585d052b163b65a6b7
Yucheng Huang <yuh032@ucsd.edu>
2023-06-20 13:55:15 -0700

    modify dotfile

--- example.zsh
+++ example.zsh
@@ -10,8 +10,6 @@
 # 
 # brainstormr=~/Projects/development/planetargon/brainstormr
 # cd $brainstormr
-<<<<<<< Updated upstream
-=======
 #
 alias hello='echo hi'
 alias hi='echo hello'
@@ -23,14 +21,12 @@ alias zsh_config='vim ~/.oh-my-zsh/custom/example.zsh'
 # alias i:='cd /mnt/i'
 # alias work='cd /mnt/f/workspace'
 alias pfoo='echo hello'
-alias tmux_boot='sh ~/storage/scripts/tmux/boot_tmux.sh'
-alias tmux_kill='sh ~/storage/scripts/tmux/tmux_kill_all.sh'
-alias tmux_reboot='sh ~/storage/scripts/tmux/tmux_reboot.sh'
-alias net_check='sh ~/storage/scripts/utils/net_check.sh'
-alias nachos='~/workspace/CSE120/nachos_sp22_AidenHuang01_Tianyue-Zhao_yuang-lin/nachos/bin/nachos'
+alias tmux_boot='sh ~/dotfile/tmux/boot_tmux.sh'
+alias tmux_kill='sh ~/dotfile/tmux/tmux_kill_all.sh'
+alias tmux_reboot='sh ~/dotfile/tmux/tmux_reboot.sh'
+alias net_check='sh ~/dotfile/utils/net_check.sh'
 alias clc='clear'
-alias sshto='~/storage/scripts/ssh/ssh_to.sh'
-alias intj='/home/yucheng/.local/share/JetBrains/Toolbox/apps/IDEA-U/ch-0/221.5080.210/bin/idea.sh'
+alias sshto='~/dotfile/ssh/ssh_to.sh'
 alias edge='microsoft-edge'
 alias o='open .'
 
@@ -38,18 +34,9 @@ alias tb='tmux_boot'
 alias tk='tmux_kill'
 alias fv='xdg-open'
 
-alias leetcode='cd ~/.leetcode'
-alias beidanci='python /User/yucheng/workspace/utils/GRE/beidanci.py'
 alias wcsng='cd ~/workspace/WCSNG'
 alias work='cd ~/workspace'
-alias blinder='cd /Users/yucheng/sp23/CSE/group-3'
-
-
-export SPOTIPY_CLIENT_ID='2a407e6bd7524a4c830f7a854f86f516'
-export SPOTIPY_CLIENT_SECRET='dff4b2d7889a48148022b9c31860b815'
-export SPOTIPY_REDIRECT_URI='https://localhost:8888/callback'
 
  [ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
 
 
->>>>>>> Stashed changes
